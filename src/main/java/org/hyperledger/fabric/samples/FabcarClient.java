3893dd1cdb438331b30124aacacb1d4c32a53047
James Taylor <jamest@uk.ibm.com>
2020-11-20 16:16:12 +0000

    [WIP] Investigate error payloads
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- src/main/java/org/hyperledger/fabric/samples/FabcarClient.java
+++ src/main/java/org/hyperledger/fabric/samples/FabcarClient.java
@@ -4,9 +4,14 @@
 
 package org.hyperledger.fabric.samples;
 
+import java.util.Objects;
 import java.nio.file.Path;
+import com.google.protobuf.ByteString;
 
+import org.hyperledger.fabric.sdk.ProposalResponse;
+import org.hyperledger.fabric.protos.peer.ProposalResponsePackage;
 import org.hyperledger.fabric.gateway.Contract;
+import org.hyperledger.fabric.gateway.ContractException;
 import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.Network;
 import org.hyperledger.fabric.gateway.Wallet;
@@ -46,9 +51,18 @@ public final class FabcarClient {
 
             contract.submitTransaction("changeCarOwner", "CAR10", "Archie");
 
-            result = contract.evaluateTransaction("queryCar", "CAR10");
+            result = contract.evaluateTransaction("queryCar", "CAR999");
             System.out.println(new String(result));
+        } catch (ContractException e) {
+            byte[] payload = e.getProposalResponses().stream()
+                .map(ProposalResponse::getProposalResponse)
+                .map(ProposalResponsePackage.ProposalResponse::getPayload)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .map(ByteString::toByteArray)
+                .orElse(null);
+            System.out.println("MESSAGE " + e.getMessage());
+            System.out.println("PAYLOAD " + new String(payload));
         }
     }
-
 }
